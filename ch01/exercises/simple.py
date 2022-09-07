27d5768cb048edd9d4a77d4935534f1b352d3394
Emily Mendez <112587094+emily-val@users.noreply.github.com>
2022-09-07 00:54:08 +0000

    simple.py

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,15 @@
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15) #this prints a definite answer when it should be infinite
+
+rate = float(input("What is the exchange rate from Euro to Dollars? "))
+amount = float(input("What is the amount you would like to exchange? "))
+total = rate*amount
+result = print(total-3)
