eb212aeaa39aaa55a34833cd332728dff9a44158
Emily Mendez <112587094+emily-val@users.noreply.github.com>
2022-09-24 04:57:41 +0000

    lab 3 (unfinished)

---
+++ README.md
@@ -0,0 +1,4 @@
+#CS110 Portfolio
+Preferred name: Emily Mendez
+Discord: emiIy#7531
+REPL url: https://replit.com/join/bvmszwuyvq-emilymendez 
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -19,8 +19,88 @@ leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
 
+michelangelo.forward(random.randrange(1, 101))
+leonardo.forward(random.randrange(1, 101))
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, 20)
+for i in range(10):
+  michelangelo.forward(random.randrange(1, 10))
+  leonardo.forward(random.randrange(1,10))
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, 20)
 
 # PART B - complete part B here
 
+import pygame
+import math
+
+color = [255, 0, 255]
+color2 = [0, 0, 0]
+offset = 100
+pygame.init()
+window = pygame.display.set_mode()
+window.fill(color2)
+
+
+class triangle:
+  num_sides = 3
+  side_length = 50
+  coords = []
+for i in range(3):
+  theta = (2.0 * math.pi * i)/triangle.num_sides
+  x = triangle.side_length * math.cos(theta) + offset
+  y = triangle.side_length * math.sin(theta) + offset
+  triangle.coords.append((x, y))
+pygame.draw.polygon(window, color, triangle.coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+window.fill(color2)
+pygame.display.flip()
+
+  
+class square:
+  num_sides = 4
+  side_length = 50
+  coords = []
+for i in range(3):
+  theta = (2.0 * math.pi * i)/square.num_sides
+  x = square.side_length * math.cos(theta) + offset
+  y = square.side_length * math.sin(theta) + offset
+  square.coords.append((x, y))
+pygame.draw.polygon(window, color, triangle.coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+window.fill(color2)
+pygame.display.flip()
+
+class hexagon:
+  num_sides = 6
+  side_length = 50
+  coords = []
+for i in range(3):
+  theta = (2.0 * math.pi * i)/hexagon.num_sides
+  x = hexagon.side_length * math.cos(theta) + offset
+  y = hexagon.side_length * math.sin(theta) + offset
+  hexagon.coords.append((x, y))
+pygame.draw.polygon(window, color, triangle.coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+window.fill(color2)
+pygame.display.flip()
+
+class nonagon: 
+  num_sides = 9
+  side_length = 50
+  coords = []
+for i in range(3):
+  theta = (2.0 * math.pi * i)/nonagon.num_sides
+  x = nonagon.side_length * math.cos(theta) + offset
+  y = nonagon.side_length * math.sin(theta) + offset
+  nonagon.coords.append((x, y))
+pygame.draw.polygon(window, color, triangle.coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+window.fill(color2)
+pygame.display.flip()
 
 window.exitonclick()
